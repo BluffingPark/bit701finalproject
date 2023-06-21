6b2e7612572d5677f436fffdf8cabea5631f1fea
Park Myungsoo <qkautnkr@gmail.com>
2023-06-21 16:21:17 +0900

    0621

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import upimg from './up.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={upimg} width={300} border="1"/>
+      <br/><br/>
+      <h2>Docker 배포 성공 기원!!!!</h2>
+    </div>
+  );
+}
+
+export default App;
