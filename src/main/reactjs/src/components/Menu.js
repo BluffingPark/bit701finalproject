acd2e00301bd37e80163f6e3b22cc7cedc91ff1b
Park Myungsoo <qkautnkr@gmail.com>
2023-06-23 17:43:41 +0900

    0623

--- src/main/reactjs/src/components/Menu.js
+++ src/main/reactjs/src/components/Menu.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import Axios from 'axios';
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import '../App.css';
 
 function Menu(props) {
+    
+        const loginStatus = sessionStorage.loginok;
+        let navLinkText = '';
+      
+        if (loginStatus === 'no') {
+          navLinkText = '로그인';
+        } else if (loginStatus ==='yes') {
+          const myName=sessionStorage.myname;
+          navLinkText = `로그아웃 ${myName}님`;
+        }
     return (
         <ul className='menu'>
             <li>
@@ -16,10 +27,26 @@ function Menu(props) {
             </li>
             <li>
                 <NavLink to={"/board/list"}>게시판</NavLink>
-            </li>
-            <li>
-                <NavLink to={"/login"}>로그인</NavLink>
-            </li>
+            </li>            
+                {
+                    sessionStorage.loginok==null || sessionStorage.loginok==='no'?
+                    <li>
+                        <NavLink to={"/login"}>로그인</NavLink>
+                    </li>:
+                    <div>
+                        <li style={{width:'200px',backgroundColor:'pink',cursor:'pointer'}} onClick={()=>{
+                            sessionStorage.removeItem("loginok");
+                            sessionStorage.removeItem("myid");
+                            sessionStorage.removeItem("myname");
+                            window.location.reload();
+                        }}>로그아웃 {sessionStorage.myname}님
+
+                        </li>
+                    </div>
+                }
+                
+            
+            
         </ul>
     );
 }
--- src/main/reactjs/src/member/LoginForm.js
+++ src/main/reactjs/src/member/LoginForm.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../App.css';
 
 function LoginForm(props) {
+    const [myid,setMyid]=useState('');
+    const [mypass,setMypass]=useState('');
+    const navi=useNavigate();
+
+    //submit 이벤트
+    const onSubmitLogin=(e)=>{
+        e.preventDefault();
+        const url=`/member/login?myid=${myid}&mypass=${mypass}`;
+        Axios.get(url)
+        .then(res=>{
+            if(res.data.success==='yes'){
+                /*
+                localStorage: 직접 지우기 전에는 브라우저에 남아있음
+                sessionStorage: 브라우저 닫으면 지워짐
+                */
+               sessionStorage.loginok="yes";
+               sessionStorage.myname=res.data.myname;
+               sessionStorage.myid=myid;
+               navi("/")
+               window.location.reload();//새로고침
+            }else{
+                alert("아이디나 비밀번호가 맞지 않습니다");
+               sessionStorage.loginok="no";
+               sessionStorage.myname="";
+               sessionStorage.myid="";
+               navi("/")
+            }
+        })
+    }
     return (
-        <div>
-            <h1>로그인 폼</h1>
+        <div className='login'>
+            <form onSubmit={onSubmitLogin}>
+            <table className='table' style={{width:'300px'}}>
+                    <caption align="top"><b>로그인</b></caption>
+                    <tbody>
+                        <tr>
+                            <th style={{width:'100px',backgroundColor:'#b0e0e6'}}>아이디</th>
+                            <td className='input-group'>
+                                <input type='text' className='form-control' style={{width:'100px'}} placeholder='아이디' required value={myid}
+                                onChange={(e)=>{
+                                setMyid(e.target.value)
+                            }}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th style={{width:'100px',backgroundColor:'#b0e0e6'}}>비밀번호</th>
+                            <td>
+                                <input type='password' className='form-control' required placeholder='비밀번호' value={mypass}
+                                onChange={(e)=>setMypass(e.target.value)}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={2} align='center'>
+                                <button type='submit' className='btn btn-outline-info' style={{width:'100px'}}>로그인</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
         </div>
     );
 }
